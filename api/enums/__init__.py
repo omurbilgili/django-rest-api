d7a9f37c238adbe8ac868d20c6dc34532867d05c
Ömür Bilgili <omurbilgili@gmail.com>
2024-08-17 18:26:06 +0300

    Enums added

---
+++ api/enums/__init__.py
@@ -0,0 +1,1 @@
+from api.enums.categoryEnum import ProductCategory
---
+++ api/enums/categoryEnum.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class ProductCategory(Enum):
+    ELECTRONICS = "Electronics"
+    OFFICE_SUPPLIES = "Office Supplies"
+    FURNITURE = "Furniture"
